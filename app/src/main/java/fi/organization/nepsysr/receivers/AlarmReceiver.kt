391aecb61053b16b1e77e90f57b449e01acc8f5d
Jussi <jussi.kanto@tuni.fi>
2021-10-25 20:38:07 +0300

    Add AlarmReceiver

---
+++ app/src/main/java/fi/organization/nepsysr/receivers/AlarmReceiver.kt
@@ -0,0 +1,86 @@
+package fi.organization.nepsysr.receivers
+
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import fi.organization.nepsysr.ProfileActivity
+import fi.organization.nepsysr.R
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    val CHANNEL_ID = "ChannelId"
+    val CHANNEL_NAME = "ChannelName"
+
+    @SuppressLint("WrongConstant")
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val notificationId = intent?.getIntExtra("notificationId", 0)
+        var title = intent?.getStringExtra("title")
+        var topic = intent?.getStringExtra("topic")
+
+        createNotificationChannel(context)
+
+        // When notification is tapped, call ProfileActivity
+        var intent = Intent(context, ProfileActivity::class.java)
+        var contentIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+        var notificationManager : NotificationManager =
+            context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        /*
+        // Prepare Notification-
+        var builder : Notification.Builder = Notification.Builder(context)
+        builder.setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle("kh")
+            .setContentText("akhdfkha")
+            .setWhen(System.currentTimeMillis())
+            .setAutoCancel(true)
+            .setContentIntent(contentIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+
+         */
+
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID) // Create notification with channel Id
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(title)
+            .setContentText(topic)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+        builder.setContentIntent(contentIntent).setAutoCancel(true)
+
+        val mNotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        with(mNotificationManager) {
+            notify(123, builder.build())
+        }
+
+        // Notify
+        //notificationManager.notify(notificationId!!, builder.build())
+
+        Log.d("TAGI", "TÄÄLLÄ")
+
+    }
+
+    private fun createNotificationChannel(context : Context?) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val mChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT.apply {
+                }
+            )
+            val notificationManager =
+                context!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+        }
+    }
+}
