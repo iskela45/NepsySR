e9b9b0353f5b40238a8f653559419c901ab79c07
Jussi <jussi.kanto@tuni.fi>
2021-10-25 20:36:34 +0300

    Add function for starting the alarm

--- app/src/main/java/fi/organization/nepsysr/AddingTaskActivity.kt
+++ app/src/main/java/fi/organization/nepsysr/AddingTaskActivity.kt
@@ -1,11 +1,23 @@
 package fi.organization.nepsysr
 
 import android.app.Activity
+import android.app.AlarmManager
+import android.app.AlarmManager.RTC_WAKEUP
+import android.app.PendingIntent
 import android.content.Intent
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.preference.PreferenceManager
+import fi.organization.nepsysr.receivers.AlarmReceiver
+import java.util.*
 
 class AddingTaskActivity : AppCompatActivity() {
 
@@ -15,6 +27,9 @@ class AddingTaskActivity : AppCompatActivity() {
     lateinit var addImage: EditText
     lateinit var saveTask: Button
 
+    private var notificationId = 1
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adding_task)
@@ -31,13 +46,57 @@ class AddingTaskActivity : AppCompatActivity() {
             val topic = editTopic.text.toString()
             val img = addImage.text.toString()
 
+            startAlarm(title, timer, topic)
+
             val data = Intent()
             data.putExtra("title", title)
             data.putExtra("timer", timer)
             data.putExtra("topic", topic)
             data.putExtra("img", img)
-            setResult(Activity.RESULT_OK, data)
+            setResult(RESULT_OK, data)
             finish()
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun startAlarm(title : String, timer : String, topic: String) {
+
+        // Retrieves the user chosen alarm time from shared preferences
+        val sp = PreferenceManager.getDefaultSharedPreferences(this)
+        var alarmSp = sp.getString("alarmTime", "")
+        var splittedTime = alarmSp!!.split(":")
+
+        val intent = Intent(this, AlarmReceiver::class.java)
+        intent.putExtra("notificationId", notificationId)
+        intent.putExtra("title", title)
+        intent.putExtra("topic", topic)
+
+        // getBroadcast(context, requestCode, intent, flags)
+        val alarmIntent : PendingIntent = PendingIntent.getBroadcast(this, 0,
+            intent,PendingIntent.FLAG_CANCEL_CURRENT)
+        val alarm : AlarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+
+        val cal = Calendar.getInstance()
+        cal.set(Calendar.HOUR_OF_DAY, splittedTime[0].toInt())
+        cal.set(Calendar.MINUTE, splittedTime[1].toInt())
+        cal.set(Calendar.SECOND, 0)
+
+        var alarmStartTime : Long = cal.timeInMillis
+
+        val calDate = Calendar.getInstance()
+        //val df = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())
+        calDate.add(Calendar.DATE, Integer.parseInt(timer))
+        //val formattedDate: String = df.format(calDate)
+
+        val today = Calendar.getInstance()
+
+        // Calculates the difference between the current date and the days given by the user
+        val difference: Long =  calDate.getTimeInMillis() - today.getTimeInMillis()
+        val days = (difference / (1000 * 60 * 60 * 24)).toInt()
+
+        Log.d("TAGI","$days")
+
+        // Set alarm (type, milliseconds, intent)
+        alarm.set(RTC_WAKEUP, alarmStartTime, alarmIntent)
     }
 }
