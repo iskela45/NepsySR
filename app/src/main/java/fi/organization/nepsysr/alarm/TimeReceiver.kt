091d59e01cd280d45fc0942e006fe714c9040b03
Jussi <jussi.kanto@tuni.fi>
2021-11-03 16:03:51 +0200

    Add timeReceiver to receive broadcast and do things as the days change

---
+++ app/src/main/java/fi/organization/nepsysr/alarm/TimeReceiver.kt
@@ -0,0 +1,32 @@
+package fi.organization.nepsysr.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import fi.organization.nepsysr.database.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlin.concurrent.thread
+
+class TimeReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        val applicationScope = CoroutineScope(SupervisorJob())
+        val database = AppRoomDatabase.getDatabase(context!!, applicationScope)
+        val alarm = AlarmHandler(context)
+
+        thread {
+            var tasks = database.appDao().getAllTasksList()
+
+            // update the daysRemain-column of the task by subtracting one day
+            for (i in tasks) {
+                database.appDao().updateDaysRemain(i.taskId)
+            }
+            // update notification
+            alarm.updateAlarm()
+        }
+    }
+
+}
