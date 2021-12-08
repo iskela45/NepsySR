3801d4cdeeaef33d79f9d3572c4a33ccc010d713
Joonas Kirsi <joonas_kirsi@hotmail.com>
2021-12-08 11:30:42 +0200

    Add button to save profile color

--- app/src/main/java/fi/organization/nepsysr/fragments/ColorPickerFragment.kt
+++ app/src/main/java/fi/organization/nepsysr/fragments/ColorPickerFragment.kt
@@ -1,9 +1,18 @@
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import fi.organization.nepsysr.R
+import android.graphics.drawable.ColorDrawable
+import java.lang.String
+
 
 class ColorPickerFragment : DialogFragment() {
     override fun onCreateView(
@@ -12,6 +21,20 @@ class ColorPickerFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         var rootView: View = inflater.inflate(R.layout.fragment_color_picker, container, false)
+
+        var submitButton = rootView.findViewById<Button>(R.id.btSubmit)
+        var colorRadioGroup = rootView.findViewById<RadioGroup>(R.id.rgColors)
+        submitButton.setOnClickListener {
+            val selectedID = colorRadioGroup.checkedRadioButtonId
+            val selectedButton = rootView.findViewById<RadioButton>(selectedID)
+            val viewColor = selectedButton.background as ColorDrawable
+            val color = viewColor.color
+            val hexColor = "#" + Integer.toHexString(color).substring(2)
+
+            Toast.makeText(context, hexColor, Toast.LENGTH_LONG).show()
+
+            dismiss()
+        }
 
         return rootView
     }
